f0df71e491f073734fdb4ba433828927a8f054ed
Rustam4457 <punk60826@gmail.com>
2018-08-03 23:35:59 +0300

    testing playing song from album page

--- includes/header.php
+++ includes/header.php
@@ -18,9 +18,11 @@ else {
 	<title>Welcome to title</title>
 
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="assets/js/script.js"></script>
 </head>
-
 <body>
+
 	<div class="mainContainer">
  <div id="topContainer">
 <?php include("includes/navBarContainer.php"); ?>
--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -1,3 +1,30 @@
+<?php
+$songQuery = mysqli_query($con, "SELECT id FROM songs ORDER BY RAND() LIMIT 10");
+
+$resultArray = array();
+
+while($row = mysqli_fetch_array($songQuery)) {
+	array_push($resultArray, $row['id']);
+}
+
+$jsonArray = json_encode($resultArray);
+?>
+
+<script>
+
+$(document).ready(function() {
+	currentPlaylist = <?php echo $jsonArray; ?>;
+	audioElement = new Audio();
+	setTrack(currentPlaylist[0], currentPlaylist, false);
+});
+
+
+function setTrack(trackId, newPlaylist, play) {
+audioElement.setTrack("assets/music/Katrina and the Waves - Walking on Sunshine.mp3");
+audioElement.audio.play();
+}
+
+</script>
 	<div id="nowPlayingBarContainer">
 
 		<div id="nowPlayingBar">
