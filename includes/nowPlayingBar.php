ac1b36482f37d7ae97788345476f610a305d32b4
Rustam4457 <punk60826@gmail.com>
2018-08-13 22:05:09 +0300

    nextSong

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -68,10 +68,20 @@ var percentage = mouse.offsetX / $(progressBar).width() * 100;
 var seconds = audioElement.audio.duration * (percentage / 100);
 audioElement.setTime(seconds);
 }
+function nextSong(){
+	if(currentIndex === currentPlaylist.length - 1){
+		currentIndex = 0;
+	}
+	else{
+		currentIndex++;
+	}
+	var trackToPlay = currentPlaylist[currentIndex];
+	setTrack(trackToPlay, currentPlaylist, true);
+}
 function setTrack(trackId, newPlaylist, play) {
 
 	$.post("includes/handlers/ajax/getSongJson.php", { songId: trackId }, function(data) {
-
+  currentIndex = currentPlaylist.indexOf(trackId);
 		var track = JSON.parse(data);
 		$(".trackName span").text(track.title);
 
@@ -163,7 +173,7 @@ function pauseSong() {
 						<img src="assets/images/icons/pause.png" alt="Pause">
 					</button>
 
-					<button class="controlButton next" title="Next button">
+					<button class="controlButton next" title="Next button" onclick="nextSong()">
 						<img src="assets/images/icons/next.png" alt="Next">
 					</button>
 
