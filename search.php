d170af4d6dffb040cc6a566bf46dff82fd2c4f66
Rustam4457 <punk60826@gmail.com>
2018-08-27 23:04:56 +0300

    searching when user stops typing

--- search.php
+++ search.php
@@ -13,6 +13,21 @@ if(isset($_GET['term'])){
 <div class="searchContainer">
 
 	<h4>Search for an artist, album or song</h4>
-	<input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing...">
+	<input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing..." onfocus="this.value = this.value">
 
 </div>
+
+<script>
+$(".searchInput").focus()		
+$(function(){
+	var timer;
+	$(".searchInput").keyup(function(){
+		clearTimeout(timer);
+		timer = setTimeout(function(){
+   var val = $(".searchInput").val();
+   openPage("search.php?term=" + val)
+		}, 2000)
+	})
+})
+
+</script>
